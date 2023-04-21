430adce9786fd08a77abfe779e2647de48773fc7
DanielaAndreaGonzalez <daniela.andrea.gonzalez@correounivalle.edu.co>
2023-04-21 14:25:33 -0500

    Se crea el componente add-pets

---
+++ src/app/refugio/components/add-pets/add-pets.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pets',
+  templateUrl: './add-pets.component.html',
+  styleUrls: ['./add-pets.component.css']
+})
+export class AddPetsComponent {
+
+}
