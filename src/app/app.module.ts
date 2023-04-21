446f7a8fcc9f4619076aa24474debfbc821b8f97
Kevin Santiago Narvaez Vera <knarvaez@excelcredit.co>
2023-04-21 14:57:16 -0500

    Se crea compomente list-pets para listar los registros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MiRefuioMainComponent } from './refugio/mi-refugio-main/mi-refuio-main.component';
 import { AddPetsComponent } from './refugio/components/add-pets/add-pets.component';
+import { ListPetsComponent } from './refugio/components/list-pets/list-pets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiRefuioMainComponent,
-    AddPetsComponent
+    AddPetsComponent,
+    ListPetsComponent
   ],
   imports: [
     BrowserModule
